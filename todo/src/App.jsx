362c88762e56b86e10c9437f1b5dec17ffbeb1c8
donghyun-0503 <donghyun100503@gmail.com>
2026-07-30 18:07:46 +0900

    reducer을 사용하여 리팩토링

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,33 +1,52 @@
 import "./App.css";
-import { useRef, useState } from "react";
+import { useRef, useReducer } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map(item =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    case "DELETE":
+      return state.filter(item => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, dispatch] = useReducer(reducer, []);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-    setTodos((prevTodos) => [newTodo, ...prevTodos]);
+  const onCreate = content => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
   };
 
-  const onUpdate = (targetId) => {
-    setTodos((prevTodos) =>
-      prevTodos.map((item) =>
-        item.id === targetId ? { ...item, isDone: !item.isDone } : item
-      )
-    );
+  const onUpdate = targetId => {
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
   };
 
-  const onDelete = (targetId) => {
-    setTodos((prevTodos) => prevTodos.filter((item) => item.id !== targetId));
+  const onDelete = targetId => {
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
   };
 
   return (
