690fca1c2b37ea887f19c27c645f70021a5a3b78
b0kuyaba <e10yc1122@gmail.com>
2026-08-09 10:16:22 +0900

    props 학습 및 코드 작성

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -3,14 +3,24 @@ import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
 import Button from "./components/Button.jsx"
+import button from "./components/Button.jsx";
 
 function App() {
+    const buttonProps = {
+        text :"메일",
+        color : "red",
+        a: 1,
+        b: 2,
+        c: 3,
+    };
 
   return (
     <>
-        <Button text = {"메일"} color = {"red"} />
+        <Button {...buttonProps}/>
         <Button text = {"카페"} />
-        <Button text = {"블로그"} />
+        <Button text = {"블로그"}>
+            <Header/>
+        </Button>
     </>
   )
 }
--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,8 +1,17 @@
-const Button = (props) => {
+const Button = ({children,text, color = "black"}) => {
+    const onClickButton = (e) => {
+        console.log(e)
+        console.log(text);
+    };
     return (
-        <button style={{color:props.color}}>
-            {props.text}
-        </button>);
+        <button
+            onClick = {onClickButton}
+            style={{color: color}}>
+            {text} - {color}
+            {children}
+        </button>
+    );
 };
+
 
 export default Button;
