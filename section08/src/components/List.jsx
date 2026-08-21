35797a38188b15ad91bbe86528080c702edaaad9
b0kuyaba <e10yc1122@gmail.com>
2026-08-21 23:08:26 +0900

    feat: Todoitem.jsx 추가 및 List.css 완성

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,7 +1,11 @@
+import './List.css'
 const List = () => {
-    return <div>
+    return (
+    <div className="List">
         <h4>Todo List🌱</h4>
-    </div>;
+        <input placeholder="검색어를 입력하세요"/>
+    </div>
+    );
 };
 
 export default List;
