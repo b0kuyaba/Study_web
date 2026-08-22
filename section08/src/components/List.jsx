04f0e46ec119dbe40640f2d524c1f4af94a0dae8
b0kuyaba <e10yc1122@gmail.com>
2026-08-22 09:19:27 +0900

    feat: Todo.jsx 추가

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,9 +1,15 @@
 import './List.css'
+import Todoitem from "./Todoitem.jsx";
 const List = () => {
     return (
     <div className="List">
         <h4>Todo List🌱</h4>
         <input placeholder="검색어를 입력하세요"/>
+        <div>
+            <Todoitem />
+            <Todoitem />
+            <Todoitem />
+        </div>
     </div>
     );
 };
