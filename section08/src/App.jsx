a0e3124443e820dfe595c1dba0fb111ee9107f56
b0kuyaba <e10yc1122@gmail.com>
2026-08-23 21:22:09 +0900

    데이터 추가

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -4,13 +4,29 @@ import Header from "./components/Header.jsx";
 import Editor from "./components/Editor.jsx";
 import List from "./components/List.jsx";
 
-function App() {
-    const mockData = [
-        {
+const mockData = [
+    {
+        id: 0,
+        isDone: false,
+        content: "React 공부하기",
+        data: new Date().getDate(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: "빨래하기",
+        data: new Date().getDate(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: "노래 연습하기",
+        data: new Date().getDate(),
+    },
+];
 
-        }
-    ]
-    const [todos, setTodos] = useState( [] )
+function App() {
+    const [todos, setTodos] = useState( mockData )
   return (
     <div className="App">
       <Header/>
