640f1b675785d7499d3c3ebd76191376e810b3ab
b0kuyaba <e10yc1122@gmail.com>
2026-08-26 20:32:38 +0900

    Read 완료

--- section08/src/components/Todoitem.jsx
+++ section08/src/components/Todoitem.jsx
@@ -2,7 +2,7 @@ import "./TodoItem.css"
 const TodoItem = ({id, isDone, content, date}) => {
     return (
         <div className="TodoItem">
-            <input checked={isDone} type="checkbox"/>
+            <input readOnly checked={isDone} type="checkbox"/>
             <div className="Content">{content}</div>
             <div className="Date">
                 {new Date(date).toLocaleDateString()}
