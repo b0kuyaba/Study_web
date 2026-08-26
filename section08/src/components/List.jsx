638650ea742bae4e8eeba963c89e5c28f27dd25b
b0kuyaba <e10yc1122@gmail.com>
2026-08-26 19:39:22 +0900

    학습 시작

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,14 +1,14 @@
 import './List.css'
 import TodoItem from "./Todoitem.jsx";
-const List = () => {
+const List = ({todos}) => {
     return (
     <div className="List">
         <h4>Todo List🌱</h4>
         <input placeholder="검색어를 입력하세요"/>
         <div className="Todos_wrapper">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {todos.map((todo) => {
+                return <div>{todo.content}</div>;
+            }) }
         </div>
     </div>
     );
