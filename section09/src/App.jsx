dc0d088663f0a203e7b2305df6af9524cbd42662
b0kuyaba <e10yc1122@gmail.com>
2026-08-29 00:01:18 +0900

    spacebar

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -30,13 +30,16 @@ function reducer (state, action){
     switch (action.type) {
         case 'CREATE':
             return [action.data, ...state];
+
         case 'UPDATE':
             return state.map((item)=>
                 item.id === action.targetId
                 ? {...item, isDone: !item.isDone}
                 : item)
+
         case 'DELETE':
             return state.filter((item) => item.id !== action.targetId)
+
         default : return state
     }
 }
