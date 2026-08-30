9f78d1ac38a60d0539a6520d3231139f8dfc31f4
b0kuyaba <e10yc1122@gmail.com>
2026-08-30 20:33:37 +0900

    section11 시작

--- section11/eslint.config.js
+++ section11/eslint.config.js
@@ -16,8 +16,6 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
-      "no-unused-vars":"off",
-      "react/prop-types":"off",
     },
   },
 ])
