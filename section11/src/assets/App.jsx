ad5eaca89f8015e8e59adafc74c84799a524e96a
b0kuyaba <e10yc1122@gmail.com>
2026-09-01 23:47:22 +0900

    App.jsx 세팅

---
+++ section11/src/assets/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Home from "./pages/Home.jsx"
+import Diary from "./pages/Diary.jsx"
+import New from "./pages/New.jsx"
+
+function App() {
+
+  return <> </>;
+}
+
+export default App
