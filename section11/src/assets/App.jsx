5e550f5d97efbecaf352a4513f8d11d5811b1996
b0kuyaba <e10yc1122@gmail.com>
2026-09-02 23:47:38 +0900

    navigate

--- section11/src/assets/App.jsx
+++ section11/src/assets/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
-import {Routes, Route, Link} from "react-router-dom";
+import {Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home.jsx"
 import Diary from "./pages/Diary.jsx"
 import New from "./pages/New.jsx"
 import Notfound from "./pages/Notfound.jsx";
 
 function App() {
+  const nav = useNavigate();
 
+  const onClickButton = () => {
+    nav("new");
+  }
   return (
       <>
         <div>
@@ -14,6 +18,9 @@ function App() {
           <Link to={"/new"}>New</Link>
           <Link to={"/diary"}>Diary</Link>
         </div>
+        <button onClick={onClickButton}>
+          New 페이지로 이동
+        </button>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/new" element={<New />}/>
