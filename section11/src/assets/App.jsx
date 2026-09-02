afc75f69d3bc1f713f0ff7c3fee1ad7af963316a
b0kuyaba <e10yc1122@gmail.com>
2026-09-02 13:26:13 +0900

    App.jsx 세팅

--- section11/src/assets/App.jsx
+++ section11/src/assets/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home.jsx"
 import Diary from "./pages/Diary.jsx"
 import New from "./pages/New.jsx"
 
 function App() {
 
-  return <> </>;
+  return <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/new" element={<New />}/>
+    <Route path="/diary" element={<Diary />}/>
+  </Routes>;
 }
 
 export default App
