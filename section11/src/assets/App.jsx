793a3e729089331f784e9756f17e5de41ba9aeca
b0kuyaba <e10yc1122@gmail.com>
2026-09-02 13:32:09 +0900

    Notfound 추가

--- section11/src/assets/App.jsx
+++ section11/src/assets/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home.jsx"
 import Diary from "./pages/Diary.jsx"
 import New from "./pages/New.jsx"
+import Notfound from "./pages/Notfound.jsx";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Route path="/" element={<Home />}/>
     <Route path="/new" element={<New />}/>
     <Route path="/diary" element={<Diary />}/>
+    <Route path="*" element={<Notfound />}/>
   </Routes>;
 }
 
