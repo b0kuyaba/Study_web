60e45d0853563bd66195bea3b688aaa8257c5a2c
b0kuyaba <e10yc1122@gmail.com>
2026-09-12 18:21:40 +0900

    DiaryList UI

--- section11/src/pages/Home.jsx
+++ section11/src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import {useSearchParams} from "react-router-dom";
+import Header from "../components/Header.jsx"
+import Button from "../components/Button.jsx"
+import DiaryList from "../components/DiaryList.jsx";
 
 const Home = () => {
-    const [params, setParams] = useSearchParams();
 
-    return <div>Home</div>
-}
+    return (
+        <div>
+            <Header
+                title={"2024년 2월"}
+                leftChild={<Button text={"<"}/>}
+                rightChild={<Button text={">"}/>}
+            />
+            <DiaryList />
+        </div>
+    );
+};
 
 export default Home;
