33e6d237807fa093169f45da7694ac4225737b8b
b0kuyaba <e10yc1122@gmail.com>
2026-09-12 19:48:18 +0900

    navigate 사용 후 재 커밋

--- section11/src/components/DiaryItem/DiaryItem.jsx
+++ section11/src/components/DiaryItem/DiaryItem.jsx
@@ -1,14 +1,20 @@
 import "./DiaryItem.css"
 import { getEmotionImage } from "../../util/get-emotion-image.js";
+import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button.jsx"
 
 const DiaryItem = ({id, emotionId, createdDate, content})=>{
+    const nav = useNavigate();
     return (
         <div className="DiaryItem">
-            <div className={`img_section img_section_${emotionId}`}>
+            <div
+                onClick={() => nav(`/diary/${id}`)}
+                className={`img_section img_section_${emotionId}`}>
                 <img src={getEmotionImage(emotionId)} alt=""/>
             </div>
-            <div className="info_section">
+            <div
+                onClick={() => nav(`/diary/${id}`)}
+                className="info_section">
                 <div className="created_date">
                     {new Date(createdDate).toLocaleDateString()}
                 </div>
@@ -17,7 +23,9 @@ const DiaryItem = ({id, emotionId, createdDate, content})=>{
                 </div>
             </div>
             <div className="button_section">
-                <Button text={"수정하기"}/>
+                <Button
+                    onClick={() => nav(`/edit/${id}`)}
+                    text={"수정하기"}/>
             </div>
         </div>
     )
