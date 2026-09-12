45b0773b11374cade7faf1b47f970f8180b539b2
b0kuyaba <e10yc1122@gmail.com>
2026-09-12 17:42:11 +0900

    onCreate

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {useReducer} from "react";
+import {useReducer, useRef} from "react";
 import {Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx"
 import Diary from "./pages/Diary.jsx"
@@ -23,14 +23,32 @@ const mockData = [
 ]
 
 function Reducer(state,action){
-    return state;
+    switch (action.type) {
+        case "CREATE": return [action.data, ...state]
+    }
 }
 
 function App() {
     const [data, dispatch] = useReducer(Reducer, mockData);
+    const idRef = useRef(3);
 
+    const onCreate = (createdDate, emotionId, content) => {
+        dispatch({
+            type:"CREATE",
+            data: {
+                id: idRef.current++,
+                createdDate,
+                emotionId,
+                content,
+            },
+        });
+    }
   return (
       <>
+          <button onClick={() => {
+              onCreate(new Date().getTime(), 1, "Hello")
+          }}
+          >일기 추 테스트</button>
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/new" element={<New />}/>
