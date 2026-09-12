c594628104a424c55e3d9f6aa6d4ccdb0471def8
b0kuyaba <e10yc1122@gmail.com>
2026-09-12 21:24:30 +0900

    EmotionItem 완료

--- section11/src/components/Editor/Editor.jsx
+++ section11/src/components/Editor/Editor.jsx
@@ -1,6 +1,33 @@
 import "./Editor.css"
+import EmotionItem from "../EmotionItem/EmotionItem.jsx";
+
+const emotionList = [
+    {
+        emotionId : 1,
+        emotionName : "완전 좋음"
+    },
+    {
+        emotionId : 2,
+        emotionName : "좋음"
+    },
+    {
+        emotionId : 3,
+        emotionName : "그럭저럭"
+    },
+    {
+        emotionId : 4,
+        emotionName : "나쁨"
+    },
+    {
+        emotionId : 5,
+        emotionName : "끔찍함"
+    },
+
+]
 
 const Editor = () => {
+    const emotionId = 1;
+
     return (
         <div className="Editor">
             <section className="date_section">
@@ -8,7 +35,16 @@ const Editor = () => {
                 <input type="date"/>
             </section>
             <section className="emotion_section">
-
+                <h4>오늘의 감정</h4>
+                <div className="emotion_list_wrapper">
+                    {emotionList.map((item) => (
+                        <EmotionItem
+                            key={item.emotionId}
+                            {...item}
+                            isSelected={item.emotionId === emotionId}
+                        />
+                    ))}
+                </div>
             </section>
             <section className="content_section"></section>
             <section className="button_section"></section>
