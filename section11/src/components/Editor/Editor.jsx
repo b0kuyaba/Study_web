31da6f7c75cd861b138d13207113d1322abf37e5
b0kuyaba <e10yc1122@gmail.com>
2026-09-13 10:56:30 +0900

    date_section

--- section11/src/components/Editor/Editor.jsx
+++ section11/src/components/Editor/Editor.jsx
@@ -1,4 +1,5 @@
 import "./Editor.css"
+import {useState} from "react";
 import EmotionItem from "../EmotionItem/EmotionItem.jsx";
 import Button from "../Button/Button.jsx";
 
@@ -26,14 +27,55 @@ const emotionList = [
 
 ]
 
+const getStringedDate = (targetDate) => {
+    let year = targetDate.getFullYear();
+    let month = targetDate.getMonth() + 1;
+    let date = targetDate.getDate();
+
+    if (month < 10) {
+        month = `0${month}`;
+    }
+    if ( date < 10) {
+        date = `0${date}`;
+    }
+
+    return `${year}-${month}-${date}`;
+};
+
 const Editor = () => {
+    const [input,setInput] = useState({
+        createdDate : new Date(),
+        emotionId: 3,
+        content: "",
+    });
+
+    const onChangeInput = (e) => {
+        console.log(e.target.name);
+        console.log(e.target.value);
+
+        let name = e.target.name;
+        let value = e.target.value;
+
+        if (name === "createdDate") {
+            value = new Date(value);
+        }
+
+        setInput({
+            ...input,
+            [name]: value,
+        })
+    };
     const emotionId = 1;
 
     return (
         <div className="Editor">
             <section className="date_section">
                 <h4>오늘의 날짜</h4>
-                <input type="date"/>
+                <input
+                    name = "createdDate"
+                    onChange={onChangeInput}
+                    value={getStringedDate(input.createdDate)}
+                    type="date"/>
             </section>
             <section className="emotion_section">
                 <h4>오늘의 감정</h4>
--- section11/src/pages/New.jsx
+++ section11/src/pages/New.jsx
@@ -1,14 +1,19 @@
 import Header from "../components/Header/Header.jsx"
 import Button from "../components/Button/Button.jsx"
 import Editor from "../components/Editor/Editor.jsx";
+import {useNavigate} from "react-router-dom";
 
 
 const New = () => {
+    const nav = useNavigate();
+
     return (
         <div>
             <Header
                 title={"새 일기 쓰기"}
-                leftChild={<Button text={"뒤로 가기"}/>}
+                leftChild={
+                    <Button onClick={() => {nav(-1)}} text={"< 뒤로 가기"}/>
+                }
             />
             <Editor />
         </div>
