9cf0505952e69d24ae0c36022b80d4426fb0da48
b0kuyaba <e10yc1122@gmail.com>
2026-09-14 23:01:17 +0900

    Viewer.jsx

--- section11/src/components/Viewer/Viewer.jsx
+++ section11/src/components/Viewer/Viewer.jsx
@@ -1,8 +1,19 @@
 import "./Viewer.css"
+import {getEmotionImage} from "../../util/get-emotion-image.js";
 
 const Viewer = () => {
+    const emotionId = 1;
+
     return (
-        <div>Viewer</div>
+        <div>
+            <section className="img_section">
+                <h4>오늘의 감정</h4>
+                <div>
+                    <img src="" alt=""/>
+                </div>
+            </section>
+            <section className="content_section"></section>
+        </div>
     )
 }
 
--- section11/src/pages/Diary.jsx
+++ section11/src/pages/Diary.jsx
@@ -1,13 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useParams,useNavigate } from "react-router-dom";
 import Viewer from "../components/Viewer/Viewer.jsx";
+import Header from "../components/Header/Header.jsx";
+import Button from "../components/Button/Button.jsx";
 
 const Diary = () => {
+    const nav = useNavigate();
     const params = useParams();
     console.log(params);
 
     return (
         <div>
-
+            <Header
+            title={"yyyy-mm-dd 기록"}
+            leftChild={
+                <Button
+                    onClick={() => {nav(- 1)}}
+                    text={"< 뒤로가기"}/>}
+            rightChild={<Button text={"수정하기"}/>}
+            />
             <Viewer />
         </div>
     );
