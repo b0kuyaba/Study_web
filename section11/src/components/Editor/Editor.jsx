636d1c686ec846e13133d5b1ca4684e0a98aa3d7
b0kuyaba <e10yc1122@gmail.com>
2026-09-19 23:54:54 +0900

    refactor: emotionList js 파일로 분리 ./util

--- section11/src/components/Editor/Editor.jsx
+++ section11/src/components/Editor/Editor.jsx
@@ -3,30 +3,7 @@ import {useState, useEffect, use} from "react";
 import {useNavigate} from "react-router-dom";
 import EmotionItem from "../EmotionItem/EmotionItem.jsx";
 import Button from "../Button/Button.jsx";
-
-const emotionList = [
-    {
-        emotionId : 1,
-        emotionName : "완전 좋음"
-    },
-    {
-        emotionId : 2,
-        emotionName : "좋음"
-    },
-    {
-        emotionId : 3,
-        emotionName : "그럭저럭"
-    },
-    {
-        emotionId : 4,
-        emotionName : "나쁨"
-    },
-    {
-        emotionId : 5,
-        emotionName : "끔찍함"
-    },
-
-]
+import {emotionList} from "../../util/constants.js";
 
 const getStringedDate = (targetDate) => {
     let year = targetDate.getFullYear();
--- section11/src/components/Viewer/Viewer.jsx
+++ section11/src/components/Viewer/Viewer.jsx
@@ -1,15 +1,23 @@
 import "./Viewer.css"
 import {getEmotionImage} from "../../util/get-emotion-image.js";
+import {emotionList} from "../../util/constants.js";
+import emotionItem from "../EmotionItem/EmotionItem.jsx";
 
 const Viewer = () => {
     const emotionId = 1;
+
+    const emotionItem = emotionList.find(
+        (item) => String(item.emotionId) === String(emotionId)
+    );
 
     return (
         <div>
             <section className="img_section">
                 <h4>오늘의 감정</h4>
                 <div>
-                    <img src="" alt=""/>
+                    <img src={getEmotionImage(emotionId)} alt=""/>
+                    <div>
+                        {emotionItem.emotionName}</div>
                 </div>
             </section>
             <section className="content_section"></section>
